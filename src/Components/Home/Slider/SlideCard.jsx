d5ff0b36a2d7deac3444ce1d05fc23ed4b5c3043
Rashed <w3rashed@gmail.com>
2024-04-28 22:55:52 +0600

    added React Simple Typewriter

--- src/Components/Home/Slider/SlideCard.jsx
+++ src/Components/Home/Slider/SlideCard.jsx
@@ -1,11 +1,23 @@
 import PropTypes from "prop-types";
+import { Typewriter } from "react-simple-typewriter";
 const SlideCard = ({ slide }) => {
   const { photo_url } = slide;
   return (
     <div className="">
       <div className=" left-[30vh] z-10 absolute top-[30%] text-white">
         <h1 className="text-4xl">Discover Haven</h1>
-        <h2 className="text-6xl">Your Journey Begins Here</h2>
+
+        <h2 className="text-6xl">
+          <Typewriter
+            words={["Your Journey Begins Here"]}
+            loop={100}
+            cursor
+            cursorStyle="_"
+            typeSpeed={30}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </h2>
         <p className="text-lg">6540 TOURS ARE AVAILABLE, BOOK NOW</p>
       </div>
       <div className="relative bg-[#273367d3]">
