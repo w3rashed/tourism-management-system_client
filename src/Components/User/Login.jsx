0f00987f989c7b26ae7812fd67948a881e87116e
Rashed <w3rashed@gmail.com>
2024-04-30 12:36:42 +0600

    log in page re design

--- src/Components/User/Login.jsx
+++ src/Components/User/Login.jsx
@@ -76,86 +76,99 @@ const Login = () => {
       <Helmet>
         <title>Login-Discover Haven</title>
       </Helmet>
+      <div
+        className="hero min-h-screen rounded-xl"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/J3pLkNr/image.png)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60 rounded-xl"></div>
+        <div className="hero-content ">
+          <div className="max-w-md">
+            <div className="card-body bg-white bg-opacity-60 rounded-xl my-10">
+              <div>
+                <h3 className="text-[#006c70] text-lg font-semibold fon">
+                  Login
+                </h3>
+                <button onClick={handleGoogleLogin} className="btn w-full mt-4">
+                  <FaGoogle></FaGoogle>
+                  Login With Google
+                </button>
+                <button onClick={handleGithubLogin} className="btn w-full my-4">
+                  <FaGithubSquare></FaGithubSquare>
+                  Login With Git hub
+                </button>
+                <div className="border-b my-4"></div>
+              </div>
+              <form onSubmit={handleLogin} className="">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="flex items-center justify-end">
+                  <div className="form-control w-full relative">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="password"
+                      className="input input-bordered"
+                      required
+                    />
+                    <label className="label">
+                      <a href="#" className="label-text-alt link link-hover">
+                        Forgot password?
+                      </a>
+                    </label>
+                  </div>
+                  <p
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute  hover:cursor-pointer mr-3"
+                  >
+                    {showPassword ? (
+                      <FaRegEye></FaRegEye>
+                    ) : (
+                      <FaRegEyeSlash></FaRegEyeSlash>
+                    )}
+                  </p>
+                </div>
+                <p className="text-[#cf2e2e]">{error}</p>
 
-      <div className="card-body">
-        <div>
-          <h3 className="text-[#006c70] text-lg font-semibold fon">Login</h3>
-          <button onClick={handleGoogleLogin} className="btn w-full mt-4">
-            <FaGoogle></FaGoogle>
-            Login With Google
-          </button>
-          <button onClick={handleGithubLogin} className="btn w-full my-4">
-            <FaGithubSquare></FaGithubSquare>
-            Login With Git hub
-          </button>
-          <div className="border-b my-4"></div>
-        </div>
-        <form onSubmit={handleLogin} className="">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              name="email"
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-end">
-            <div className="form-control w-full relative">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                name="password"
-                type={showPassword ? "text" : "password"}
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <p
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute  hover:cursor-pointer mr-3"
-            >
-              {showPassword ? (
-                <FaRegEye></FaRegEye>
-              ) : (
-                <FaRegEyeSlash></FaRegEyeSlash>
-              )}
-            </p>
-          </div>
-          <p className="text-[#cf2e2e]">{error}</p>
-
-          <div className="form-control mt-6">
-            <button
-              className="py-3 rounded-lg bg-[#ff5a5f] text-white hover:bg-transparent hover:text-[#ff5a5f]
+                <div className="form-control mt-6">
+                  <button
+                    className="py-3 rounded-lg bg-[#ff5a5f] text-white hover:bg-transparent hover:text-[#ff5a5f]
            hover:border-2 hover:border-[#ff5a5f] duration-300"
-            >
-              Login
-            </button>
+                  >
+                    Login
+                  </button>
+                </div>
+                <div>
+                  <p className="text-center mt-4">
+                    Dont you have an account?{" "}
+                    <Link
+                      to="/register"
+                      // state={{ from: location.state.from }}
+                      replace
+                      className="text-[#cf2e2e] font-semibold hover:border-b-2 border-[#ff5a5f] duration-100"
+                    >
+                      Register
+                    </Link>
+                  </p>
+                </div>
+              </form>
+            </div>
           </div>
-          <div>
-            <p className="text-center mt-4">
-              Dont you have an account?{" "}
-              <Link
-                to="/register"
-                // state={{ from: location.state.from }}
-                replace
-                className="text-[#cf2e2e] font-semibold hover:border-b-2 border-[#ff5a5f] duration-100"
-              >
-                Register
-              </Link>
-            </p>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
